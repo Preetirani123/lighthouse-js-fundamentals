511b0e474e9473ae293df8df7bb52f594658e30d
Preetirani123 <rani.preeti3333@gmail.com>
2021-01-09 21:28:16 +0000

    function exercise

--- chorus-for.js
+++ chorus-for.js
@@ -1,5 +1,25 @@
-const chorus = "Let's dance!";
-for (let repeat = 0; repeat < 10; repeat++) {
-  console.log(chorus);
+// const chorus = "Let's dance!";
+// for (let repeat = 0; repeat < 10; repeat++) {
+//   console.log(chorus);
+// }
+// console.log("Until the sun comes up!");
+
+
+for (let i = 100; i <=200; i++){
+
+  if ( i % 3 == 0 && i % 4 == 0){
+    console.log("Loopy Lighthouse")
+  }
+
+  else if (i % 3 == 0){
+    console.log("Loopy")
+  }
+
+  else if ( i % 4 == 0){
+    console.log("Lighthouse")
+  }
+
+  else{
+    console.log(i)
+  }
 }
-console.log("Until the sun comes up!");
