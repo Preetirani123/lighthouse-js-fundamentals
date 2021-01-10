1d116515911d150a4a70f44d8c9cdc8e2b6a87ab
Preetirani123 <rani.preeti3333@gmail.com>
2021-01-10 06:35:43 +0000

    all array questions solved

---
+++ ingredients.js
@@ -0,0 +1,15 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+//using for loop//
+console.log("Ingredients list:")
+for(var i = 0; i <ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+//using while loop//
+console.log("Ingredients list:")
+var i = 0;
+while(i < ingredients.length){
+  console.log(ingredients[i]);
+  i++
+}
